f32817629cae6ff1df79f392bccb8dc7a6d629cd
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-01-17 20:01:39 +0530

    build fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,7 @@
 import React,{useContext} from "react";
-import { Link } from "react-router-dom";
-import { listcontext } from "../App";
-
 
 export function Header(){
 
-    let context = useContext(listcontext);
     
     return(
         <div className="head">
